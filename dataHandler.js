ff53d5c633a0212159d8cdd9aad8c7915332635d
unknown <luciajastephen@gmail.com>
2022-02-26 00:52:13 +0800

    Prepping the slash cmd handler pre-deployment

--- dataHandler.js
+++ dataHandler.js
@@ -3,12 +3,16 @@ async function createCmd(client, guildId) {
         {
             name: "ping",
             description: "Serves a bot ping"
+        }, 
+        {
+            name: "test",
+            description: "Test command handler!"
         }
     ]
     
     // You can comment out which do you want to distribute your slash commands.
-    //await client.guilds.cache.get(guildId)?.commands.set(data) // Server-exclusive
-    await client.application?.commands.set(data) // Globally (perfect for public bots)
+    await client.guilds.cache.get(guildId)?.commands.set(data) // Server-exclusive
+    //await client.application?.commands.set(data) // Globally (perfect for public bots)
 }
 
 module.exports = { createCmd }
--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,8 @@ const client = require('../index.js').client;
 const { createCmd } = require('../dataHandler')
 client.on('ready', () => {
     console.log('Hello! The bot is now ready.')
-    createCmd(client)
+
+    // Inside the double quotation marks, you can enter your server ID.
+    // Otherwise, you can remove it to be deployed globally.
+    createCmd(client, "")
 })
